73451dc441a5b79386f060147a7e2c578bdeaa20
vgianluca96 <gianluca.vallese@hotmail.com>
2023-10-25 16:04:48 +0200

    aggiunto margin al jumbo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 
 <div class="container py-4">
 
-    <div class="jumbotron">
+    <div class="jumbotron my-4">
         <h1 class="display-4">Hello, {{$name}}!</h1>
         <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
         <hr class="my-4">
